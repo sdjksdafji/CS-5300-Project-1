77f07745d8d53201dd286c95219846a9073dd35d
sw773 <sw773@EN-CS-LGG23-09.cs.cornell.edu>
2014-03-09 19:51:15 -0400

    buggy

--- src/main/java/course/cs5300/project1a/service/SessionCookieService.java
+++ src/main/java/course/cs5300/project1a/service/SessionCookieService.java
@@ -1,6 +1,5 @@
 package course.cs5300.project1a.service;
 
-<<<<<<< HEAD
 import java.sql.Timestamp;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,13 +12,5 @@ public interface SessionCookieService {
 
 	public void createSession(HttpServletResponse response,
 			Timestamp currentTimestamp, long version);
-=======
-import javax.servlet.http.HttpServletRequest;
 
-import course.cs5300.project1a.pojo.SessionContent;
-
-public interface SessionCookieService {
-	public SessionContent getSession(HttpServletRequest request);
-	
->>>>>>> branch 'master' of https://github.com/sdjksdafji/CS-5300-Project-1a
 }
