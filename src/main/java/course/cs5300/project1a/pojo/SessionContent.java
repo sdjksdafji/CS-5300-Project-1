f5152c04bdfaaddeb55aa4b43f0c82bcda7817ed
sdjksdafji <sdjksdafji@gmail.com>
2014-03-12 21:45:55 -0400

    session size limit added

--- src/main/java/course/cs5300/project1a/pojo/SessionContent.java
+++ src/main/java/course/cs5300/project1a/pojo/SessionContent.java
@@ -3,9 +3,13 @@ package course.cs5300.project1a.pojo;
 import java.sql.Timestamp;
 
 public class SessionContent {
-	private String message;
-	private long version;
-	private Timestamp expirationTimestamp;
+	private String message; // 512 - 8 - 8 = 496 bytes. internal encoding is 2
+							// bytes for each char
+	private long version; // 64 bit = 8 bytes
+	private Timestamp expirationTimestamp; // getTime() is a long; 64 bits = 8
+											// bytes
+
+	private static int STRING_SIZE_LIMIT = 248;
 
 	public SessionContent() {
 		super();
@@ -31,7 +35,11 @@ public class SessionContent {
 	}
 
 	public void setMessage(String message) {
-		this.message = message;
+		if (message.length() <= STRING_SIZE_LIMIT) {
+			this.message = message;
+		} else {
+			this.message = message.substring(0, STRING_SIZE_LIMIT);
+		}
 	}
 
 	public long getVersion() {
