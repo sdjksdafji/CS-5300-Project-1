1729e82563d7da812f0b173d1b5c540402fc15ed
sdjksdafji <sdjksdafji@gmail.com>
2014-03-23 15:18:15 -0400

    test case added

---
+++ src/test/java/course/cs5300/project1a/service/implementation/VersionManagerImplTest.java
@@ -0,0 +1,19 @@
+package course.cs5300.project1a.service.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class VersionManagerImplTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
