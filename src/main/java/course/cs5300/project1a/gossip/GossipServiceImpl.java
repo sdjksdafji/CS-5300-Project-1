abf51aac6d0af0bb1cd8002df0564ba5876d1407
Shuyi Wang <sdjksdafji@gmail.com>
2014-03-27 01:15:54 -0400

    finished: gossip

--- src/main/java/course/cs5300/project1a/gossip/GossipServiceImpl.java
+++ src/main/java/course/cs5300/project1a/gossip/GossipServiceImpl.java
@@ -1,9 +1,13 @@
 package course.cs5300.project1a.gossip;
 
 import java.net.InetAddress;
+import java.util.Random;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import course.cs5300.project1a.dao.BootstrapViewDAO;
 import course.cs5300.project1a.pojo.View;
@@ -14,6 +18,7 @@ public class GossipServiceImpl implements GossipService {
 
 	private static View view = new View();
 	private static final int VIEW_SIZE = 5;
+	private static final int GOSSIP_SECS = 60;
 	
 	@Inject
 	private BootstrapViewDAO bootstrapViewDAO;
@@ -61,12 +66,27 @@ public class GossipServiceImpl implements GossipService {
 	public void updateBootstrapView() {
 		// TODO Auto-generated method stub
 		View bootstrapView = this.bootstrapViewDAO.getBootstrapView();
-		bootstrapView.union(view);
+		View copy = this.getView();
+		bootstrapView.union(copy);
 		bootstrapView.insert(this.getLocalIPService.getLocalIP());
 		bootstrapView.shrink(VIEW_SIZE);
 		this.bootstrapViewDAO.setBootstrapView(bootstrapView);
 	}
 	
-	
+	@Scheduled(initialDelay = 10000, fixedRate = java.lang.Long.MAX_VALUE)
+	@Async
+	public void runUpdateBootstrapView(){
+		System.out.println("Updating bootstrap view");
+		 while(true) {
+			 this.updateBootstrapView();
+			 Random generator = new Random();
+			 try {
+				Thread.sleep( (GOSSIP_SECS/2) + generator.nextInt( GOSSIP_SECS ) );
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 }
+	}
 
 }
