3c3c556ea0908d8c234540c12dc8ff2ae6ea0d1c
Shuyi <Shuyi@Shuyi-X220i>
2014-04-13 17:04:55 -0400

    added: display gossip view on web page

--- src/main/java/course/cs5300/project1a/jsfbean/ServerInfoBean.java
+++ src/main/java/course/cs5300/project1a/jsfbean/ServerInfoBean.java
@@ -1,19 +1,40 @@
 package course.cs5300.project1a.jsfbean;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.context.annotation.Scope;
 
+import course.cs5300.project1a.gossip.GossipService;
+import course.cs5300.project1a.pojo.View;
 import course.cs5300.project1a.service.GetLocalIPService;
 
 @Named
 @Scope("request")
 public class ServerInfoBean {
-	String ipAddress;
+	private String ipAddress;
+	private List<String> peerIpList;
 
 	@Inject
 	private GetLocalIPService getLocalIpService;
+	
+	@Inject
+	private GossipService gossipService;
+	
+	@PostConstruct
+	public void init(){
+		View view = this.gossipService.getView();
+		peerIpList = new ArrayList<String>();
+		for(InetAddress ip:view.getIpAddresses()){
+			peerIpList.add(ip.toString());
+		}
+	}
 
 	public String getIpAddress() {
 		this.ipAddress = this.getLocalIpService.getLocalIP().toString();
@@ -23,4 +44,14 @@ public class ServerInfoBean {
 	public void setIpAddress(String ipAddress) {
 		this.ipAddress = ipAddress;
 	}
+
+	public List<String> getPeerIpList() {
+		return peerIpList;
+	}
+
+	public void setPeerIpList(List<String> peerIpList) {
+		this.peerIpList = peerIpList;
+	}
+	
+	
 }
