d6c80c6d39009f3356226a28720cbd68af70a556
GeekShawnMa <SuperXiao@rrdhcp-10-32-231-45.redrover.cornell.edu>
2014-04-12 21:06:04 -0400

    Commit

--- src/main/java/course/cs5300/project1a/service/implementation/LocalSessionTableManagerImpl.java
+++ src/main/java/course/cs5300/project1a/service/implementation/LocalSessionTableManagerImpl.java
@@ -1,5 +1,7 @@
 package course.cs5300.project1a.service.implementation;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
@@ -7,12 +9,14 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import course.cs5300.project1a.jsfbean.ServerInfoBean;
 import course.cs5300.project1a.pojo.SessionContent;
 import course.cs5300.project1a.pojo.SessionID;
 import course.cs5300.project1a.service.LocalSessionTableManager;
@@ -20,42 +24,77 @@ import course.cs5300.project1a.service.LocalSessionTableManager;
 @Named
 @Scope("singleton")
 public class LocalSessionTableManagerImpl implements LocalSessionTableManager {
-
+	private static Map<SessionID, SessionContent> sessionMap = new HashMap<SessionID, SessionContent>();
+	private static long currentSessionId = 0;
+	@Inject
+	private ServerInfoBean serverInfoBean;
 	@Override
 	public SessionID addSession(SessionContent sessionContent) {
 		// TODO Auto-generated method stub
-		return null;
+		long sessionNumber = Math.max(sessionContent.getVersion(), currentSessionId++);
+		currentSessionId = sessionNumber+1;
+		SessionID newSessionID = null;
+		try {
+			InetAddress addr = InetAddress.getByName(serverInfoBean.getIpAddress().toString());
+			newSessionID = new SessionID(sessionNumber,addr);
+			sessionMap.put(newSessionID, (SessionContent) sessionContent.clone());
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newSessionID;
 	}
 
 	@Override
 	public void updateSession(SessionID sessionId, SessionContent sessionContent) {
 		// TODO Auto-generated method stub
-		
+		try {
+			sessionMap.put(sessionId, (SessionContent) sessionContent.clone());
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public SessionContent getSession(SessionID sessionId) {
 		// TODO Auto-generated method stub
-		return null;
+		return sessionMap.get(sessionId);
 	}
 
 	@Override
 	public void removeSession(SessionID sessionId) {
 		// TODO Auto-generated method stub
+		sessionMap.remove(sessionId);
 		
 	}
 
 	@Override
 	public void removeExpiredSession() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Removing Expired Session  <<-------------------------------------");
+		Timestamp currentTs = new Timestamp((new Date()).getTime());
+		Iterator<Entry<SessionID, SessionContent>> iter = this.sessionMap.entrySet().iterator();
+		while(iter.hasNext()){
+			Entry<SessionID, SessionContent> entry = iter.next();
+			SessionContent sessionContent = entry.getValue();
+			if(sessionContent.getExpirationTimestamp().before(currentTs)){
+				// ---------------------------------------------------
+				System.out.println(entry.getKey() + "Expire Time" + entry.getValue().getExpirationTimestamp().toString() + " current time " + currentTs.toString() + " removed from session table <<------------------------------");
+				// ---------------------------------------------------
+				iter.remove();
+			}
+		}
 	}
 
 	@Override
 	public void storeSessionLocally(SessionID sessionId,
 			SessionContent sessionContent) {
 		// TODO Auto-generated method stub
-		
+		updateSession(sessionId,sessionContent);
 	}
 	
 	
